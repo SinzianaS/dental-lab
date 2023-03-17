c933f0413adf1701a1bc64f80eb216bdf0d38e5d
Sinziana Sovejanu <27846889+SinzianaS@users.noreply.github.com>
2023-03-17 12:17:24 +0200

    modified Patient model

---
+++ src/main/java/com/dental/lab/model/Patient.java
@@ -0,0 +1,25 @@
+package com.dental.lab.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "patients")
+public class Patient {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "uuid2")
+    private UUID id;
+
+    private String name;
+
+    private int age;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Dentist dentist;
+
+}
