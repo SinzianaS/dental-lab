6487e6b1c2f8415e93a04c9da6e2fc9b9ebf85d5
Sinziana Sovejanu <27846889+SinzianaS@users.noreply.github.com>
2023-03-21 20:56:19 +0200

    changed path

--- src/main/java/com/dental/lab/controller/DentistController.java
+++ src/main/java/com/dental/lab/controller/DentistController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/dentists")
+@RequestMapping("/api/dentists")
 @CrossOrigin(origins = "http://localhost:3000")
 @Transactional
 public class DentistController {
